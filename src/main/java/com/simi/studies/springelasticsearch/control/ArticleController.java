f72ca0216ffb678723a7d9e00f88a37ef6b90544
Felipe Miranda <felip.simi@gmail.com>
2020-08-12 01:40:21 -0300

    Quinto passo: criacao da controller para busca por titulo

--- src/main/java/com/simi/studies/springelasticsearch/control/ArticleController.java
+++ src/main/java/com/simi/studies/springelasticsearch/control/ArticleController.java
@@ -2,9 +2,14 @@ package com.simi.studies.springelasticsearch.control;
 
 import com.simi.studies.springelasticsearch.database.ArticleRepository;
 import com.simi.studies.springelasticsearch.model.Article;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Optional;
 
 @RestController
 public class ArticleController {
@@ -18,6 +23,14 @@ public class ArticleController {
   @PostMapping("/articles")
   public Article create(@RequestBody final Article article) {
     return repository.save(article);
+  }
+
+  @GetMapping("/articles")
+  public Page<Article> getByTitle(@RequestParam(required = false) final String title,
+                                  @RequestParam(required = false)
+                                  final Optional<Pageable> pageableParam) {
+    final var pageable = pageableParam.orElse(Pageable.unpaged());
+    return repository.findByTitle(title, pageable);
   }
 
 }
